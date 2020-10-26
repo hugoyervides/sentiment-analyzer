c7f1c262706fb12f53d65f8e5e80d8af67a7cf36
Victor Oyervides <victor.covarrubias@hotmail.com>
2020-10-25 21:09:26 -0600

    init

---
+++ project/bot_middle_east.py
@@ -0,0 +1,33 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sentiment_engine as s
+
+#consumer key, consumer secret, access token, access secret.
+ckey=""
+csecret=""
+atoken=""
+asecret=""
+class listener(StreamListener):
+    def on_data(self, data):
+        all_data = json.loads(data)
+        tweet = all_data["text"]
+        sentiment_value, confidence = s.sentiment(tweet)
+        print(tweet, sentiment_value, confidence)
+        
+        if confidence*100 >= 80:
+            output = open("data/twitter-middle_east.txt","a")
+            output.write(sentiment_value)
+            output.write('\n')
+            output.close()
+
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(locations=[-17.1411042264,18.753774966,71.3730433703,37.384138605])
